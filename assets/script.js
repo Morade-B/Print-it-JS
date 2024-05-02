60f28630fbabb1874445d4a9a26c529d4795edbf
Morade B <morade.baghloul@gmail.com>
2024-05-02 17:14:37 +0200

    correction html et déclaration des variables pour le projet

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,37 @@
+
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-// Ajout des event listener sur les flèches 
+//   Déclaration des variables pour l'ensemble du projet  
 
-let flècheGauche = document.getElementById("flècheGauche")
+const dots = document.querySelector(".dots");
+const flècheDroite = document.querySelector(".arrow_right");
+const flècheGauche = document.querySelector(".arrow_left");
+const img = document.querySelector('.banner-img');
+const text = document.querySelector('#banner p');
+let index = 0;
 
- flècheGauche.addEventListener("click", () => {
-	slideIndex--;
-	console.log("Vous avez cliqué sur la flèche de Gauche")
-})
 
-let flècheDroite = document.getElementById("flècheDroite")
 
- flècheDroite.addEventListener("click", () => {
-	slideIndex++;
-	console.log("Vous avez cliqué sur la flèche de Droite")
-})
+
+
+
+
 
 
